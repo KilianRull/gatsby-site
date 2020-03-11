9e1c04b33251c057ca94fcf3cddc99b04ecabd10
Kilian Rullkötter <kilian@kjubo.com>
2020-03-11 15:14:43 +0100

    basic implementation styled components for testing

---
+++ src/components/Section.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const SectionWrap = styled.div`
+    height: 100vh;
+    background-color: grey;
+`
+
+
+const Section = props => (
+    <SectionWrap>
+
+    </SectionWrap>
+
+)
+
+export default Section
