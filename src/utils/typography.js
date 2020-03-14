13bcf6dff8699c1eadfb2434aeb49540d603785e
Kilian Rullkötter <kilian@kjubo.com>
2020-03-14 10:40:15 +0100

    added custom font via gatsby-source-filesystem

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -4,14 +4,14 @@ const typography = new Typography({
   baseFontSize: "18px",
   baseLineHeight: 1.666,
   headerFontFamily: [
-    "Avenir Next",
+    "Suisse Sign",
     "Helvetica Neue",
     "Segoe UI",
     "Helvetica",
     "Arial",
     "sans-serif",
   ],
-  bodyFontFamily: ["Georgia", "serif"],
+  bodyFontFamily: ["Suisse Sign", "sans-serif"],
 })
 
 export default typography
